f5a9578876262503d8d996f9cc03e38eff44a315
wduntak <wduntak@gmail.com>
2014-11-24 16:41:59 -0500

    Created a scroll function in products.js, send alert when scroll to bottom

--- app/assets/javascripts/products.js
+++ app/assets/javascripts/products.js
@@ -1,5 +1,11 @@
 
 $(document).on('ready page:load', function(){
+	$(window).scroll(function(){
+		if ($(window).scrollTop() > $(document).height() - $(window).height() - 50){
+			return alert('near bottom');
+		}
+	});
+
 	$('#search-form').submit(function(event) {
 		event.preventDefault();
 		var searchValue = $('#search').val();
