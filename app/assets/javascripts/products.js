319a21dda41bc46b1fdc56c7881f0e0c49faf7c1
wduntak <wduntak@gmail.com>
2014-11-24 13:38:20 -0500

    Added .get shorthand

--- app/assets/javascripts/products.js
+++ app/assets/javascripts/products.js
@@ -3,15 +3,21 @@
 
 $(document).on('ready page:load', function(){
 	$('#search-form').submit(function(event) {
-    event.preventDefault();
-    var searchValue = $('#search').val();
+		event.preventDefault();
+		var searchValue = $('#search').val();
 
-    $.ajax({
-      url: '/products?search=' + searchValue,
-      type: 'GET',
-      dataType: 'html'
-    }).done(function(data){
-      $('#products').html(data);
-    });
-  });
+		// $.ajax({
+		// 	url: '/products?search=' + searchValue,
+		// 	type: 'GET',
+		// 	dataType: 'html'
+		// }).done(function(data){
+		// 	$('#products').html(data);
+		// });
+
+		$.get('/products?search=' + searchValue)
+      		.done(function(data){
+        		console.log(data);
+        		$('#products').html(data);
+      		});
+	});
 });
